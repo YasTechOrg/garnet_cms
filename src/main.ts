fa9c15d42950b155e15d1f36d2329238250e4ec7
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-05-03 03:24:10 +0430

    Commit Dist

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "./assets/vendors/uicons-regular-rounded.css"
 import "font-awesome/css/font-awesome.min.css"
 
+import.meta.env.BASE_URL = "/garnet_cms/"
 const app = createApp(App)
 app.use(Particles)
 app.mount('#app')
